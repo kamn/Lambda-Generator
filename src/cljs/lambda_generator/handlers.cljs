d60e7c65037990782a897411a87c481e435ca7dc
Kamn <kamn@users.noreply.github.com>
2016-09-07 23:03:38 -0600

    Added all tools to be upgradeable with lps

--- src/cljs/lambda_generator/handlers.cljs
+++ src/cljs/lambda_generator/handlers.cljs
@@ -2,15 +2,30 @@
     (:require [re-frame.core :as re-frame]
               [lambda-generator.db :as db]))
 
+(defn get-tool-lps [key db]
+  (let [tool      (get-in db [key :count])
+        tool-lps  (get-in db [key :lps])]
+    (* tool tool-lps)))
+
 ;;TODO Move out
 (defn re-calc-lps [db]
-  (let [tool-1     (get-in db [:tool-1 :count])
-        tool-1-lps (get-in db [:tool-1 :lps])
-        tool-2     (get-in db [:tool-2 :count])
-        tool-2-lps (get-in db [:tool-2 :lps])]
+  (let [tool-1-lps (get-tool-lps :tool-1 db)
+        tool-2-lps (get-tool-lps :tool-2 db)
+        tool-3-lps (get-tool-lps :tool-3 db)
+        tool-4-lps (get-tool-lps :tool-4 db)
+        tool-5-lps (get-tool-lps :tool-5 db)
+        tool-6-lps (get-tool-lps :tool-6 db)
+        tool-7-lps (get-tool-lps :tool-7 db)
+        tool-8-lps (get-tool-lps :tool-8 db)]
     (/ (js/Math.round (* 10 (+
-      (* tool-1-lps tool-1)
-      (* tool-2-lps tool-2)))) 10)))
+      tool-1-lps
+      tool-2-lps
+      tool-3-lps
+      tool-4-lps
+      tool-5-lps
+      tool-6-lps
+      tool-7-lps
+      tool-8-lps))) 10)))
 
 (re-frame/register-handler
  :initialize-db
